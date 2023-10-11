8fe90ddc528ac952492db5502fdd64f18c36a47b
sinatajeddini <sinatajeddini@gmail.com>
2023-10-11 11:16:20 +0330

    C#1jifgrjei

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(a+b);
+
